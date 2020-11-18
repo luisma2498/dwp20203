6ef7dcef2b418eb34cc492897af14579cc28a546
17040422 <71669701+17040422@users.noreply.github.com>
2020-11-17 21:10:45 -0600

    Productos act
    Lista de productos actualizada

--- src/views/Producto/Producto.vue
+++ src/views/Producto/Producto.vue
@@ -18,9 +18,9 @@
                   <ion-img :src="item.src"></ion-img>
                   </ion-item>
                 
-                 <h2>Precio $ {{ item.precio }}</h2>
+                 <h2>Precio $: {{ item.precio }}</h2>
                 <ion-label>{{ item.text }}</ion-label>
-                <h3>Referencia: {{ item.referencia }} </h3>
+                <h3>Código: {{ item.referencia }} </h3>
                 
                 <ion-button expand="full">COMPRAR</ion-button>
               </ion-card-content>
@@ -75,25 +75,69 @@ export default defineComponent({
   setup() {
     const items = [
       {
-        title: "Anillo X",
-        text: "Anillo de oro",
-        referencia: "ZVB1",
-        precio: "3500",
+        
+        title: "Anillo de compromiso plata",
+        text: "22k",
+        referencia: "H6N3",
+        precio: "25,500",
+        src: "/Img/compromiso.jpg",
+      },
+      {
+        title: "Anillo de oro",
+        text: "14k",
+        referencia: "AD3V1",
+        precio: "15,500",
         src: "/Img/anillo.jpg",
       },
       {
-        title: "Anillo Y",
-        text: "Anillo de plata",
+        title: "Anillo de oro, diamante esmeralda",
+        text: "10k",
         referencia: "ZV21",
-        precio: "3500",
+        precio: "20,500",
         src: "/Img/anillo2.jpg",
       },
+     
+       {
+        title: "Pulsera oro rigida",
+        text: "5k",
+        referencia: "K6B0",
+        precio: "1,500",
+        src: "/Img/pulsera.jpg",
+      },
       {
-        title: "Anillo Z",
-        text: "Collar de plata ",
-        referencia: "Z1B1",
-        precio: "3500",
-        src: "/Img/collar.jpg",
+        title: "Brazalete de pulsera de alambre expandible de oro",
+        text: "8k",
+        referencia: "P925",
+        precio: "2,500",
+        src: "/Img/brazalete.jpg",
+      },
+        {
+        title: "Brazalete cupido, bronce baño de oro",
+        text: "22k",
+        referencia: "V9K7",
+        precio: "12,500",
+        src: "/Img/brazaletecupido.jpg",
+      },
+      {
+        title: "Reloj Michael Kors DAMA",
+        text: "28k",
+        referencia: "M6K8",
+        precio: "20,000",
+        src: "/Img/reloj.jpg",
+      },
+       {
+        title: "Reloj para hombres Michael Kors",
+        text: "30k",
+        referencia: "M8K1",
+        precio: "25,000",
+        src: "/Img/relojh.jpg",
+      },
+       {
+        title: "Reloj Michael Kors DAMA",
+        text: "25k",
+        referencia: "M5K1",
+        precio: "17,000",
+        src: "/Img/relojm.jpg",
       },
     ];
     return { items };
